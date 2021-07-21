644065fed378b55d9958567992b4634daa428407
Jerome <ducampdorgasjerome@gmail.com>
2021-07-21 18:06:25 +0200

    adresse api

--- js/Ajax.js
+++ js/Ajax.js
@@ -1,11 +1,11 @@
 //fonction fillNounours
 async function fillNounours() {
-  try{
-    await fetch('http://localhost:3000/api/teddies') // will return info, but in wrong format
+  try {
+    await fetch('https://p5backend.netlify.app/api/teddies') // will return info, but in wrong format
       .then((response) => response.json()) // will return info, in json format
       .then((nounours) => remplirListeNounours(nounours)) // main code here, using json info
   }
-  catch(error){
+  catch (error) {
     console.log("oups: " + error)
   }
 }
