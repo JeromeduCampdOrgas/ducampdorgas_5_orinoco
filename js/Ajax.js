00fcc302ac0d06c006a9b6338e8fff062d0acdef
Jerome <ducampdorgasjerome@gmail.com>
2021-07-21 18:31:55 +0200

    test adresse

--- js/Ajax.js
+++ js/Ajax.js
@@ -1,7 +1,7 @@
 //fonction fillNounours
 async function fillNounours() {
   try {
-    await fetch('https://p5backend.netlify.app/api/teddies') // will return info, but in wrong format
+    await fetch('orinocobackend.herokuapp.com/api/teddies') // will return info, but in wrong format
       .then((response) => response.json()) // will return info, in json format
       .then((nounours) => remplirListeNounours(nounours)) // main code here, using json info
   }
