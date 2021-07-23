d8c2dcef9489667e8cd5aec69487c93db113163d
Jerome <ducampdorgasjerome@gmail.com>
2021-07-23 15:30:26 +0200

    port 3000

--- js/Ajax.js
+++ js/Ajax.js
@@ -1,7 +1,7 @@
 //fonction fillNounours
 async function fillNounours() {
   try {
-    await fetch('orinocobackend.herokuapp.com/api/teddies') // will return info, but in wrong format
+    await fetch('http://localhost:3000/api/teddies') // will return info, but in wrong format
       .then((response) => response.json()) // will return info, in json format
       .then((nounours) => remplirListeNounours(nounours)) // main code here, using json info
   }
